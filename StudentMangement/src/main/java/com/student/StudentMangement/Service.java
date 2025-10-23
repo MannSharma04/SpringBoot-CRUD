d7c3611d7e917170c7729c1b16a3d0f39d3f8518
Your Name <you@example.com>
2025-10-23 17:33:38 +0530

    PUT Api

--- StudentMangement/src/main/java/com/student/StudentMangement/Service.java
+++ StudentMangement/src/main/java/com/student/StudentMangement/Service.java
@@ -3,6 +3,7 @@ package com.student.StudentMangement;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @org.springframework.stereotype.Service
 public class Service
@@ -17,6 +18,16 @@ public class Service
 
     public Student addstudent(Student student)
     {
+        return studentRepository.save(student);
+    }
+
+    public Student updatestudent(int id , Student student)
+    {
+        Student S1 = studentRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Student not found with id: " + id));
+        S1.setName(student.getName());
+        S1.setAge(student.getAge());
+        S1.setCourse(student.getCourse());
         return studentRepository.save(student);
     }
 }
--- StudentMangement/src/main/java/com/student/StudentMangement/StudentController.java
+++ StudentMangement/src/main/java/com/student/StudentMangement/StudentController.java
@@ -1,5 +1,6 @@
 package com.student.StudentMangement;
 
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +15,25 @@ public class StudentController {
     private Service service;
 
 
-    @GetMapping
+    @GetMapping("/getallstudents")
     public List<Student> getallStudent() {
         return service.getallstudent();
     }
-    @PostMapping
+    @PostMapping("/createstudent")
     public ResponseEntity<Student> addStudent(@RequestBody Student student)
     {
         student = service.addstudent(student);
 
         return new ResponseEntity< >(student, HttpStatus.CREATED);
     }
+    @PutMapping("/{Id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable(value = "Id") int id , @RequestBody Student newstudent)
+    {
+    Student Updatedstudent = service.updatestudent(id , newstudent);
+    return new ResponseEntity< >(Updatedstudent, HttpStatus.CREATED);
+    }
+
+
 
 
 }
